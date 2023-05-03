3a32a7cff2308e6f5bcf6c1a6aaaa7a1b4c62ecb
Stevenlub <lubbesteven27@gmail.com>
2023-05-03 14:48:25 +0200

    first commit

---
+++ README.txt/#READ ME.py
@@ -0,0 +1,14 @@
+#READ ME 
+
+# This site is for a political Candidate, (Joe Biden) its an website that can stock
+#and resell Biden Merchandise as well as get in contact with his campaign.
+
+#ADMIN LOGIN DETAILS
+
+# username - adminn
+# password - lubbe 
+
+#USER LOGIN DETAILS
+
+# username - Jason 
+# password - Vuuren#13
